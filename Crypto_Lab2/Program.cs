d23e0272bdfb5af924b2ae2b226fcb443b19c933
Danko <wally19@bk.ru>
2019-09-15 19:31:00 +0400

    Lab_2 tests

--- Crypto_Lab2/Program.cs
+++ Crypto_Lab2/Program.cs
@@ -6,18 +6,6 @@ namespace Crypto_Lab2
     {
         static void Main(string[] args)
         {
-            var rnd = new Random(19);
-
-            var block = new byte[8];
-            var key = new byte[16];
-
-            rnd.NextBytes(block);
-            rnd.NextBytes(key);
-
-            var encrypter = new TEAEncrypter(key);
-            var decrypter = new TEADecrypter(key);
-            var encrypt = encrypter.Encrypt(block);
-            var decrypt = decrypter.Decrypt(encrypt);
         }
     }
 }
---
+++ Tests/Crypto_Lab2_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Crypto_Lab2;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Crypto_Lab2_Tests
+    {
+        [Test]
+        public void ShouldEncryptAndDecryptFullBlockByTEA()
+        {
+            var rnd = new Random(19);
+            var block = new byte[8];
+            var key = new byte[16];
+            rnd.NextBytes(block);
+            rnd.NextBytes(key);
+
+            var encrypter = new TEAEncrypter(key);
+            var decrypter = new TEADecrypter(key);
+            var encrypt = encrypter.Encrypt(block);
+            var decrypt = decrypter.Decrypt(encrypt);
+
+            decrypt.Should().BeEquivalentTo(block, o => o.WithStrictOrdering());
+        }
+    }
+}
