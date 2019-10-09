4eb381dc4503499cc09ea2e0de4de7d52fb7f068
Daniil Mardanov <d.mardanov@skbkontur.ru>
2019-10-09 10:14:50 +0400

    Lab3 now works with files, especially bmp

--- Crypto_Lab3/Program.cs
+++ Crypto_Lab3/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
+using CryptoCommon;
 
 namespace Crypto_Lab3
 {
@@ -10,15 +13,55 @@ namespace Crypto_Lab3
         static void Main(string[] args)
         {
             var key = File.ReadAllBytes("key");
-            var sBlock = new SBlock(key);
+            var fileName = args.Single();
+            var fileContent = new LazyFileReader(fileName);
+            var prefix = fileContent.Take(2).ToArray();
+            var cryptAsBmp = false;
+            if (prefix[0] == 0x42 && prefix[1] == 0x4D)
+                cryptAsBmp = IsCryptAsBmp();
 
-            foreach (var k in sBlock.GetBytes())
+            IEnumerable<byte> result = null;
+            var rc4 = new RC4lAgorithm();
+            if (cryptAsBmp)
             {
-                var keyChar = Console.ReadKey(true).KeyChar;
-                var b = Convert.ToByte(keyChar);
-                var e = b ^ k;
-                Console.Write(Convert.ToChar(e));
+                var byteOffset = fileContent.Skip(10).Take(4).ToArray();
+                var intOffset =BitConverter.ToInt32(byteOffset, 0);
+                result = fileContent
+                    .Take(intOffset)
+                    .Concat(rc4.Crypt(
+                        fileContent.Skip(intOffset), key));
             }
+            else result = rc4.Crypt(fileContent, key);
+            
+            var fileWriter = new LazyFileWriter(GenerateFileName(fileName));
+            fileWriter.Write(result);
+        }
+
+        private static bool IsCryptAsBmp()
+        {
+            while (true)
+            {
+                Console.WriteLine("File looks like BMP. Keep file structure unchanged? (y/n)");
+                var input = Console.ReadLine();
+                if (input == "Y" || input == "y")
+                    return true;
+                if (input == "N" || input == "n")
+                    return false;
+            }
+        }
+
+        private static string GenerateFileName(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            if (!string.IsNullOrWhiteSpace(extension))
+                fileName = fileName.Replace($"{extension}", "");
+
+            fileName = $"{fileName}_ crypted_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}";
+
+            if (!string.IsNullOrWhiteSpace(extension))
+                fileName = $"{fileName}{extension}";
+
+            return fileName;
         }
     }
 }
