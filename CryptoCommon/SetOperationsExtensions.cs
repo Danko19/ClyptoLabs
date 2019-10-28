f4b74ddad5b3ee626d0ef1e3f3d15b7ee15342db
Daniil Mardanov <d.mardanov@skbkontur.ru>
2019-10-28 20:26:08 +0400

    SHA512 preparing

---
+++ CryptoCommon/SetOperationsExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CryptoCommon
+{
+	public static class SetOperationsExtensions
+	{
+		public static byte[] Xor(this byte[] first, byte[] second)
+		{
+			if (first.Length != second.Length)
+				throw new ArgumentException(
+					$"Arrays have different sizes. First: {first.Length}, second {second.Length}");
+
+			var result = new byte[first.Length];
+			for (var i = 0; i < first.Length; i++)
+			{
+				result[i] = (byte)(first[i] ^ second[i]);
+			}
+
+			return result;
+		}
+
+		public static ulong Rotr(this ulong number, int shifts)
+		{
+			var copy = number << 64 - shifts % 64;
+			return copy + number >> shifts;
+		}
+
+		public static ulong Xor(this ulong num1, ulong num2)
+		{
+			return num1 ^ num2;
+		}
+	}
+}
--- Crypto_Lab4/SHA512.cs
+++ Crypto_Lab4/SHA512.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CryptoCommon;
+using MoreLinq;
 
 namespace Crypto_Lab4
 {
@@ -12,7 +14,42 @@ namespace Crypto_Lab4
 
 		public byte[] GetHash(IEnumerable<byte> content)
 		{
-			var h = InitializeVector();
+			var hs = InitializeVector();
+			var blockWorker = new BlockWorker(128);
+
+			foreach (var block in blockWorker.GetBlocks(content, new SHA512PaddingStandard()))
+			{
+				var blockArray = block.ToArray();
+				var subBlocks = new ulong[64];
+				for (var i = 0; i < 16; i++)
+					subBlocks[i] = BitConverter.ToUInt64(blockArray, i * 8);
+				for (var i = 16; i < 64; i++)
+				{
+					var e0 = subBlocks[i - 15];
+					var e1 = subBlocks[i - 2];
+					var s0 = e0.Rotr(7).Xor(e0.Rotr(18)).Xor(e0 >> 3);
+					var s1 = e1.Rotr(17).Xor(e1.Rotr(19)).Xor(e1 >> 10);
+					unchecked
+					{
+						subBlocks[i] = subBlocks[i - 16] + s0 + subBlocks[i - 7] + s1;
+					}
+				}
+
+				var a = hs[0];
+				var b = hs[1];
+				var c = hs[2];
+				var d = hs[3];
+				var e = hs[4];
+				var f = hs[5];
+				var g = hs[6];
+				var h = hs[7];
+
+				for (var i = 0; i < 80; i++)
+				{
+
+				}
+			}
+
 			return null;
 		}
 
